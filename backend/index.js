a8886fdb148559604bf40f53ed5367948772a90b
Arun <vikrantkumarprajapati4@gmail.com>
2024-09-20 17:48:35 +0530

    api link updated

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: "https://jobportal-live.onrender.com",
   credentials: true,
 };
 
