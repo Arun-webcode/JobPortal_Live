59196777212e53dfaa16bfe264526c0a41110c9f
Arun <vikrantkumarprajapati4@gmail.com>
2024-09-20 16:49:20 +0530

    Production ready app

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,12 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 dotenv.config({});
 
 const app = express();
 
+const _dirname = path.resolve();
 ///Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -30,10 +32,10 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-// ("http://localhost:8000/api/v1/user/register");
-// ("http://localhost:8000/api/v1/user/login");
-// ("http://localhost:8000/api/v1/user/profile/update");
-// ("http://localhost:8000/api/v1/company")
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   connectDB();
